5a034ff88a1baaa442403bd81e80968977eacc47
Anonymous-Technopath <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-01-06 13:26:15 +0530

    Added Valid_Parentheses.java

---
+++ Valid_Parentheses.java
@@ -0,0 +1,44 @@
+/*
+*Solution By : Nimish Shandilya
+*LinkedIn : https://www.linkedin.com/in/nimish-shandilya-0a429b22b
+*
+*Problem : Valid Parentheses
+*Problem Source : LeetCode
+*Problem Link : https://leetcode.com/problems/valid-parentheses/
+* 
+*/
+
+class Valid_Parentheses {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0)
+        return false;
+        ArrayList<Character> arr=new ArrayList<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='{'||s.charAt(i)=='('||s.charAt(i)=='['){
+                arr.add(s.charAt(i));
+                System.out.print(1);
+            }
+            else{
+                char c;
+                if(s.charAt(i)=='}')
+                c='{';
+                else if(s.charAt(i)==')')
+                c='(';
+                else
+                c='[';
+                if(arr.size()!=0 && arr.get(arr.size()-1)==c){
+                    arr.remove(arr.size()-1);
+                    System.out.print(2);
+                }
+                else{
+                    System.out.print(3);
+                    return false;
+                }
+            }
+        }
+        if(arr.size()==0){
+            return true;
+        }
+        return false;
+    }
+}
