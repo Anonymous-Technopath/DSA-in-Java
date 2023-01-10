39fc3795f09c96f111f237d883012198b2b12b99
Anonymous-Technopath <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-01-10 10:16:42 +0530

    Added Square_Root.java

---
+++ Square_Root.java
@@ -0,0 +1,16 @@
+/*
+*Solution By : Nimish Shandilya
+*LinkedIn : https://www.linkedin.com/in/nimish-shandilya-0a429b22b
+*
+*Problem : Square Root
+*Problem Source : Leet Code
+*Problem Link : https://leetcode.com/problems/sqrtx/
+* 
+*/
+
+
+class Square_Root {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
