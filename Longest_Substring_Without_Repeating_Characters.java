d2f7af95bd7ca9cbe6e3a4671b5a00fe22fc6794
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-06-02 10:04:07 +0530

    Update Longest_Substring_Without_Repeating_Characters.java

--- Longest_Substring_Without_Repeating_Characters.java
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -3,29 +3,34 @@
 *LinkedIn : https://www.linkedin.com/in/nimish-shandilya-0a429b22b
 *
 *Problem : Longest Substring Without Repeating Characters
-*Problem Source : Leet Code
-*Problem Link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+*Problem Source : Coding Ninjas
 * 
 */
 
-class Longest_Substring_Without_Repeating_Characters {
-    public int lengthOfLongestSubstring(String s) {
-    int result = 0;
-    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-    
-    if(s.length() == 0) return 0;
-    
-    for(int i = 0, j = 0; i < s.length(); i++) {
+import java.util.* ;
+import java.io.*; 
+public class Longest_Substring_Without_Repeating_Characters {
         
-        char ch = s.charAt(i);
-        if(map.containsKey(ch) == true) {
-            j = Math.max(j, map.get(ch) + 1);
-        }
-        
-        map.put(ch, i);
-        result = Math.max(result, i - j + 1);
-    }
-    
-    return result;
-    }
+	public static int lengthOfLongestSubstring(String s) {
+		// Write your code here.
+		int n=1,l=0,r=1;
+		if(s.length()==0){
+			return 0;
+		}
+		boolean t=false;
+		HashSet<Character> hs=new HashSet<>();
+		hs.add(s.charAt(0));
+		while(r<s.length()){
+			if(!hs.contains(s.charAt(r))){
+				hs.add(s.charAt(r));
+				n=Math.max(hs.size(),n);
+				r++;
+			}else{
+				hs.remove(s.charAt(l));
+				l++;
+			}
+		}
+		
+		return n;
+	}
 }
