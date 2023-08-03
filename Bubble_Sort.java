43a4dcfb83f9f1ff4588ef9b2867580223e542ee
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2023-08-03 21:40:40 +0530

    Created Bubble_Sort.java

---
+++ Bubble_Sort.java
@@ -0,0 +1,25 @@
+/*
+*Solution By : Nimish Shandilya
+*LinkedIn : https://www.linkedin.com/in/nimish-shandilya-0a429b22b
+*
+*Problem : Bubble Sort
+*Problem Source :Coding Ninjas
+* 
+*/
+
+
+public class Bubble_Sort {
+    public static void bubbleSort(int[] arr, int n) {
+        //Your code goes here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+}
