cda87e698e4f2c20c067bcf251968d7ab8b3c211
Nimish Shandilya <93417822+Anonymous-Technopath@users.noreply.github.com>
2024-01-26 22:39:49 +0530

    Method 2 without stack

--- Count_Of_Greater_Elements_To_The_Right.java
+++ Count_Of_Greater_Elements_To_The_Right.java
@@ -11,31 +11,14 @@ import java.util.*;
 public class Count_Of_Greater_Elements_To_The_Right {
     public static int[] countGreater(int []arr, int []query) {
         // Write your code here.
-        int sol[]=new int[arr.length];
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i:query){
-            hs.add(i);
-        }
-        Stack<Integer> s1=new Stack<>();
-        Stack<Integer> s2=new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            while(!s1.isEmpty()&&arr[s1.peek()]<arr[i]){
-                int n=s1.pop();
-                sol[n]++;
-                s2.push(n);
-            }
-            if(hs.contains(i)){
-                s1.push(i);
-            }
-            
-            while(!s2.isEmpty()){
-                s1.push(s2.pop());
-            }
-        }
-        int ans[]=new int[query.length];
+        int sol[]=new int[query.length];
         for(int i=0;i<query.length;i++){
-            ans[i]=sol[query[i]];
+            for(int j=query[i];j<arr.length;j++){
+                if(arr[j]>arr[query[i]]){
+                    sol[i]++;
+                }
+            }
         }
-        return ans;
+        return sol;
     }
 }
